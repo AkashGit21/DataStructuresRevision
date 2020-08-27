51255e7051785410b2795a44d7d45c94d13b3864
Akash Gupta <akashgzb98@gmail.com>
2020-08-27 19:44:27 +0530

    Find Shortest Path from S to E on grid using BFS

--- index.cpp
+++ index.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 #include "Graphs/graph.cpp"
 #include "Trees/tree.cpp"
+#include "Grid/grid.cpp"
 
 int main(){
 	Graph G1; int startingNode=1, endingNode=3;
-	G1.takeInput();
+	//G1.takeInput();
 	/*cout<<"\nDFS: ";	
 	int numOfComponents = G1.dfs(startingNode);
 	cout<<"\nNumber of Connected Components: "<<numOfComponents;
 	*/
-
+	/*
 	cout<<"\nBFS: ";
 	vector<int> prev = G1.bfs(startingNode);
 	cout<<"\nPath: ";
@@ -20,6 +21,10 @@ int main(){
 	vector<int> path = G1.reconstructPathFromBFS(prev,
 		startingNode, endingNode);
 	for(int node:path) cout<<" "<<node;
+	*/
 
+	Grid G;
+	G.takeInput();
+	cout<<"Distance: "<<G.bfsOnGrid(1,1);
 	return 0;
 }
